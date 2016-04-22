41a121dfe11c34cda2d1fec0d4ea9e7b77be69d0
Lars Birkmose Jensen <lars@birkmose-jensen.dk>
2016-04-22 21:09:39 +0200

    Tilføjet Ticket Machine og Ticket klasser

---
+++ Ticket.java
@@ -0,0 +1,33 @@
+
+/**
+ * Lav en beskrivelse af klassen Ticket her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class Ticket
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int price;
+
+    /**
+     * Konstruktør for objekter af klassen Ticket
+     */
+    public Ticket()
+    {
+        // initialiser instansvariable
+        price = 100;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @param  y  eksempel på en parameter til en metode
+     * @return    summen af x og y 
+     */
+    public int getPrice()
+    {
+        // indsæt din egen kode her
+        return price;
+    }
+}
---
+++ TicketMachine.java
@@ -0,0 +1,22 @@
+
+/**
+ * Lav en beskrivelse af klassen TicketMachine her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class TicketMachine
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private Ticket ticket;
+
+    /**
+     * Konstruktør for objekter af klassen TicketMachine
+     */
+    public TicketMachine()
+    {
+        // initialiser instansvariable
+        ticket = new Ticket();
+    }
+
+}
