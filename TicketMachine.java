67302f26826462fc124ae7a877a88dcbc8afbbbd
Bombemose <lars@birkmose-jensen.dk>
2016-04-22 21:41:18 +0200

    Improved TicketMachine class

--- TicketMachine.java
+++ TicketMachine.java
@@ -9,6 +9,7 @@ public class TicketMachine
 {
     // instansvariabler - erstat eksemplet herunder med dine egne variabler
     private Ticket ticket;
+    private int total;
 
     /**
      * Konstruktør for objekter af klassen TicketMachine
@@ -17,6 +18,21 @@ public class TicketMachine
     {
         // initialiser instansvariable
         ticket = new Ticket();
+    }
+    
+    public void insertMoney(int amount) {
+        total += amount;
+    }
+    
+    public boolean buyTicker() {
+        if(ticket.getPrice() <= total) {
+            total -= ticket.getPrice();
+            return true;
+        }
+        else {
+            System.out.println("Not enough money for ticket!");
+            return false;
+        }
     }
 
 }
